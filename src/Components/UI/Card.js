0ef4acdf861e54183c997c2a84e3c9e59511e366
Ntu002 <ngoctubn2002@gmail.com>
2022-11-24 03:14:04 +0700

    first commit

---
+++ src/Components/UI/Card.js
@@ -0,0 +1,20 @@
+// import './Card.css'
+
+// function Card(props) {
+//     const classes = 'card' + props.className;
+//     return <div className = {classes}> {props.children}</div>
+
+// }
+
+// export default Card;
+
+import './Card.css';
+
+const Card = (props) => {
+//function Card(props) {
+    const classes = 'card ' + props.className;
+    // phai 'card ' thi moi dung con neu 'card' thi se bi sai!!!
+    return <div className = {classes}> {props.children}</div>
+}
+
+export default Card;
