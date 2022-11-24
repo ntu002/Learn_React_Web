c65fe3d8d0f6208866d168b6074c71dd55d2f41c
ntu002 <ngoctubn2002@gmail.com>
2022-11-24 11:24:57 +0700

    test

--- src/Components/Expenses/ExpenseItem.js
+++ src/Components/Expenses/ExpenseItem.js
@@ -4,6 +4,10 @@ import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 const ExpenseItem = (props) => {
 //function ExpenseItem(props) {
+
+    const clickHander = () => {
+        console.log('Click!!!!!');
+    }
     return (
         <Card className = 'expense-item'>
             <ExpenseDate date = {props.date} />
@@ -11,6 +15,7 @@ const ExpenseItem = (props) => {
                 <h2>{props.title}</h2>
                 <div className = 'expense-item__price'> ${props.amount} </div>
             </div>
+            <button onClick = {clickHander}>Change click</button>
         </Card>
     );
 }
