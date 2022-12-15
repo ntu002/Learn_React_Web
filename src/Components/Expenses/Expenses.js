6e010dd3543eb204760a3f9119786c1505115e45
ntu002 <ngoctubn2002@gmail.com>
2022-12-15 23:51:21 +0700

    Test

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -2,29 +2,57 @@ import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
-//import Card from '../UI';
 import Card from "../UI/Card";
-import ExpensesFilter from "../NewExpense/ExpensesFilter";
+import ExpensesFilter from "./ExpensesFilter";
+
+// const Expenses = (props) => {
+//   //function Expenses(props) {
+
+//   const [filteredYear, setFilteredYear] = useState("2020");
+
+//   const filterChangeHandler = (selectedYear) => {
+//     // console.log("Expense.js");
+//     // console.log(selectedYear);
+//     setFilteredYear(selectedYear);
+//   };
+
+//   return (
+//     <div>
+//       <Card className="expenses">
+//         <ExpensesFilter
+//           selected={filteredYear}
+//           onChangeFilter={filterChangeHandler}
+//         />
+
+//         {props.items.map((expense) => (
+//           <ExpenseItem
+//             title={expense.title}
+//             amount={expense.amount}
+//             date={expense.date}
+//           />
+//         ))}
+//       </Card>
+//     </div>
+//   );
+// };
+
+// export default Expenses;
+
 
 const Expenses = (props) => {
-  //function Expenses(props) {
-
-  const [filteredYear, setFilteredYear] = useState("2020");
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   const filterChangeHandler = (selectedYear) => {
-    console.log("Expense.js");
-    console.log(selectedYear);
     setFilteredYear(selectedYear);
   };
 
   return (
     <div>
-      <Card className="expenses">
+      <Card className='expenses'>
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-
         {props.items.map((expense) => (
           <ExpenseItem
             title={expense.title}
@@ -32,14 +60,6 @@ const Expenses = (props) => {
             date={expense.date}
           />
         ))}
-
-        {/* {props.items.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))} */}
       </Card>
     </div>
   );
