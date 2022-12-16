ca0462087c660c5298964b960fa1e1c4ba9b7262
ntu002 <ngoctubn2002@gmail.com>
2022-12-16 15:17:46 +0700

    completed 1st

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -5,56 +5,28 @@ import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 
-// const Expenses = (props) => {
-//   //function Expenses(props) {
-
-//   const [filteredYear, setFilteredYear] = useState("2020");
-
-//   const filterChangeHandler = (selectedYear) => {
-//     // console.log("Expense.js");
-//     // console.log(selectedYear);
-//     setFilteredYear(selectedYear);
-//   };
-
-//   return (
-//     <div>
-//       <Card className="expenses">
-//         <ExpensesFilter
-//           selected={filteredYear}
-//           onChangeFilter={filterChangeHandler}
-//         />
-
-//         {props.items.map((expense) => (
-//           <ExpenseItem
-//             title={expense.title}
-//             amount={expense.amount}
-//             date={expense.date}
-//           />
-//         ))}
-//       </Card>
-//     </div>
-//   );
-// };
-
-// export default Expenses;
-
-
 const Expenses = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020');
+  //function Expenses(props) {
+
+  const [filteredYear, setFilteredYear] = useState("2020");
 
   const filterChangeHandler = (selectedYear) => {
+    // console.log("Expense.js");
+    // console.log(selectedYear);
     setFilteredYear(selectedYear);
   };
 
   return (
     <div>
-      <Card className='expenses'>
+      <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
+
         {props.items.map((expense) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
