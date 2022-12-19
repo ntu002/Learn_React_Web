1799134f0eafd18d562c88340fadd3d1b618d1da
ntu002 <ngoctubn2002@gmail.com>
2022-12-20 02:18:41 +0700

    20/12/2022 lan 4

--- src/Components/NewExpense/NewExpense.js
+++ src/Components/NewExpense/NewExpense.js
@@ -1,23 +1,43 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import ExpenseForm from './ExpenseForm';
-import './NewExpense.css';
+import ExpenseForm from "./ExpenseForm";
+import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
 
-    const saveExpenseDataHander = (enteredExpenseData) => {
-        const expenseData = {
-            ...enteredExpenseData,
-            id: Math.random().toString()
-        };
-        props.onAddExpense(expenseData);
-        //console.log(expenseData);
+  const saveExpenseDataHander = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
     };
+    props.onAddExpense(expenseData);
+    //console.log(expenseData);
+    setIsEditing(false); // quay ve trang thai ban dau
+  };
 
-    return <div className = "new-expense">
-        <ExpenseForm onSaveExpenseData={saveExpenseDataHander} />
-        <form></form>
+  const startEditingHander = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHander = () => {
+    setIsEditing(false);
+  };
+
+  return (
+    <div className="new-expense">
+      {!isEditing && (
+        <button onClick={startEditingHander}>Add new Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHander}
+          onCancel={stopEditingHander}
+        />
+      )}
+      <form></form>
     </div>
+  );
 };
 
 export default NewExpense;
